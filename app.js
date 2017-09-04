ba1ae33d889d07438b3f40243fe14cb40018bd12
Shivansh Rustagi <shivrustagi221@gmail.com>
2017-09-03 18:56:12 -0700

    fixed auth issue #2: false login

--- app.js
+++ app.js
@@ -30,16 +30,10 @@ var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-//require routes
-var index = require('./routes/routes');
-app.use('/', index);
-
-//various requires and utilities
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -49,16 +43,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 app.use(passport.session());
 
+//require routes
+var index = require('./routes/routes');
+app.use('/', index);
 
-//errors
+// catch 404 error and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
+// error handler
 app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
--- routes/routes.js
+++ routes/routes.js
@@ -4,31 +4,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/',
-  function(req, res) {
-    res.render('index', { title: 'Express', user: req.user });
-  });
+	function(req, res) {
+		res.render('index', { title: 'Express', user: req.user });
+	});
 
 router.get('/login',
-  function(req, res){
-    res.render('501');
-  });
-  
-router.post('/login', 
-  passport.authenticate('local', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
-  });
-  
+	function(req, res) {
+		res.render('login');
+	});
+
+router.post('/login',
+	passport.authenticate('local', { failureRedirect: '/login' }),
+	function(req, res) {
+		res.redirect('/');
+	});
+
 router.get('/logout',
-  function(req, res){
-    req.logout();
-    res.redirect('/');
-  });
+	function(req, res) {
+		req.logout();
+		res.redirect('/');
+	});
 
 router.get('/members',
-  require('connect-ensure-login').ensureLoggedIn(),
-  function(req, res){
-    res.render('members', { user: req.user });
-  });
+	require('connect-ensure-login').ensureLoggedIn(),
+	function(req, res) {
+		res.render('members');
+	});
 
 module.exports = router;
